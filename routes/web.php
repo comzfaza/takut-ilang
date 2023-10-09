3accb45294edb86685561d8bbe53ffee46add8d6
Wildan A <115684223+comzfaza@users.noreply.github.com>
2023-10-09 09:39:56 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\wildanController;
+use App\Http\Controllers\masyarakatController;
+use App\Http\Controllers\petugasController;
+
+
+
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Route::get('/coba', function () {
+    //return 'CBAIN';
+//});
+
+//Route::get('home', function () {
+    //return view('home');
+//});
+
+//Route::get('isi', function () {
+  //return view('isi');
+//});
+
+//Route::get('login', function () {
+  //return view('login');
+//});
+
+Route::get('/home', [wildanController::class,'index']);
+
+Route::post('/isi', [wildanController::class,'tambah_pengaduan']);
+
+Route::get('masyarakat', [masyarakatController::class,'rakyat']);
+
+Route::get('petugas', [petugasController::class,'petugas']);
+
+Route::get('loginmasyarakat', [masyarakatController::class,'register']);
+
+Route::get('logtugas', [petugasController::class,'login']);
+
+Route::post('/hapus-pengaduan/{id}', [wildanController::class,'hapus']);
+
+Route::post('/detail-pengaduan/{id}', [wildanController::class,'detail']);
+
+Route::get('/home/detail_ngadu/{id}', [wildanController::class,'detail_ngadu']);
+
+Route::get('isi', function () {
+    return view('isi');
+});
